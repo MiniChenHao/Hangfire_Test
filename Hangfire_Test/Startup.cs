b455c9962442fad8b5add6cdc2982337622cf3fb
MiniChenHao <2523076936@qq.com>
2020-05-19 14:21:02 +0800

    ADD
    UseDashboardMetric

--- Hangfire_Test/Startup.cs
+++ Hangfire_Test/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using Hangfire.Console;
 using Hangfire.Dashboard;
+using Hangfire.SqlServer;
 using Microsoft.Owin;
 using Owin;
 
@@ -14,6 +15,18 @@ namespace Hangfire_Test
         {
             // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
             GlobalConfiguration.Configuration.UseSqlServerStorage("ShenOnlineJob").UseConsole();
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.ServerCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(SqlServerStorage.ActiveConnections);
+            GlobalConfiguration.Configuration.UseDashboardMetric(SqlServerStorage.TotalConnections);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.RecurringJobCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.RetriesCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.AwaitingCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.EnqueuedAndQueueCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.ScheduledCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.ProcessingCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.SucceededCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.FailedCount);
+            GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.DeletedCount);
             BasicAuthAuthorizationFilter filter = new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions
             {
                 SslRedirect = false,//是否将所有非SSL请求重定向到SSL URL
