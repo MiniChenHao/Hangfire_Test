4b3c1148f4bda442cd2092107286814e3b5a48a9
MiniChenHao <2523076936@qq.com>
2020-05-20 10:26:16 +0800

    UPDATE

--- Hangfire_Test/Startup.cs
+++ Hangfire_Test/Startup.cs
@@ -15,6 +15,7 @@ namespace Hangfire_Test
         {
             // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
             GlobalConfiguration.Configuration.UseSqlServerStorage("ShenOnlineJob").UseConsole();
+
             GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.ServerCount);
             GlobalConfiguration.Configuration.UseDashboardMetric(SqlServerStorage.ActiveConnections);
             GlobalConfiguration.Configuration.UseDashboardMetric(SqlServerStorage.TotalConnections);
@@ -27,23 +28,22 @@ namespace Hangfire_Test
             GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.SucceededCount);
             GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.FailedCount);
             GlobalConfiguration.Configuration.UseDashboardMetric(DashboardMetrics.DeletedCount);
+
             BasicAuthAuthorizationFilter filter = new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions
             {
-                SslRedirect = false,//是否将所有非SSL请求重定向到SSL URL
-                RequireSsl = false,//需要SSL连接才能访问HangFire Dahsboard。强烈建议在使用基本身份验证时使用SSL
-                LoginCaseSensitive = true,//登录检查是否区分大小写
-                Users = new[] {
-                    new BasicAuthAuthorizationUser {
-                        Login = "Admin",
-                        // Password as SHA1 hash
-                        // Password = new byte[] { 0x7c,0x4a,0x8d,0x09,0xca,0x37,0x62,0xaf,0x61,0xe5,0x95,0x20,0x94,0x3d,0xc2,0x64,0x94,0xf8,0x94,0x1b }//密码
-                        PasswordClear = "123456"
-                    }
-                }
+                SslRedirect = false,
+                RequireSsl = false,
+                LoginCaseSensitive = true,
+                Users = new[] { new BasicAuthAuthorizationUser { Login = "Admin", PasswordClear = "123456" } }
             });
+            // SslRedirect          是否将所有非SSL请求重定向到SSL URL
+            // RequireSsl           需要SSL连接才能访问HangFire Dahsboard。强烈建议在使用基本身份验证时使用SSL
+            // LoginCaseSensitive   登录检查是否区分大小写
+            // Password             密码 SHA1 hash 【Password = new byte[] { 0x7c,0x4a,0x8d,0x09,0xca,0x37,0x62,0xaf,0x61,0xe5,0x95,0x20,0x94,0x3d,0xc2,0x64,0x94,0xf8,0x94,0x1b }】
+
             DashboardOptions options = new DashboardOptions { Authorization = new[] { filter } };
             app.UseHangfireDashboard("/Hangfire", options);
-            //app.UseHangfireServer();
+            app.UseHangfireServer();
         }
     }
 }
